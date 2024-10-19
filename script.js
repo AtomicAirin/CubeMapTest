2321c6442043510243763a178c126eeddd70e5eb
AtomicAirin <theatomiccharlie@gmail.com>
2024-10-19 00:35:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ async function fetchSectors() {
     // Populate dropdown with options
     for (const sector of sectors) {
         const option = document.createElement('option');
-        option.value = sector.url; // Set the value to the URL
+        option.value = sector.imagePath; // Set the value to the URL
         option.textContent = sector.name; // Set the displayed text
         sectorDropdown.appendChild(option);
     }
